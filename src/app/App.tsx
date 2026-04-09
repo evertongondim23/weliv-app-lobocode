f7be9b64e57e8c905555a5dfc85a9d2902c91d05
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2026-04-09 17:29:11 -0300

    fix: alterações app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
+import React from 'react';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AuthProvider } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
 import { Toaster } from './components/ui/sonner';
-
 export default function App() {
   return (
     <AuthProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,11 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
   
